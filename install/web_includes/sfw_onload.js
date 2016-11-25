02b98c3ba2cfd3c230a52c5c3c3adb5240d93445
Chuck Jungmann <chuckj60@gmail.com>
2016-11-25 09:01:08 -0600

    begin_app() to delegate creating object wrappers.

--- install/web_includes/sfw_onload.js
+++ install/web_includes/sfw_onload.js
@@ -2,15 +2,19 @@ window.onload = function()
 {
    function begin_app()
    {
-      if ("get_properties_info" in SFW)
-         alert(SFW.get_properties_info().join("\n"));
+      // if ("get_properties_info" in SFW)
+      //    alert(SFW.get_properties_info(SFW).join("\n"));
       
       if ("sfwanchor" in document)
       {
-         var el = document.sfwanchor;
-         var className = el.getAttribute("data-sfw-class");
-         if (className in SFW)
-            SFW.pageobj = new SFW[className](el, window.XMLDocument);
+         var director = SFW.get_director(SFW.xmldoc);
+         if (director)
+            SFW.pageobj = director;
+         
+         // var el = document.sfwanchor;
+         // var className = el.getAttribute("data-sfw-class");
+         // if (className in SFW)
+         //    SFW.pageobj = new SFW[className](el, SFW.xmldoc);
       }
 
       SFW.setup_event_handling();
