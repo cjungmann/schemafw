0c2e56e87c247694d6d615b008eb1c1c450adc8e
Chuck Jungmann <chuckj60@gmail.com>
2016-11-25 08:56:44 -0600

    fixed get_properties_info()

--- install/web_includes/sfw_debug.js
+++ install/web_includes/sfw_debug.js
@@ -2,14 +2,14 @@ function init_SFW_Debug()
 {
    var reFuncParams = /^[^\(]+\(([^\)]*)\)/;
 
-   SFW.get_properties_info = function()
+   SFW.get_properties_info = function(obj)
    {
       var arr = [];
-      var type, func, params;
-      for (prop in SFW)
+      var type, func, params, prop;
+      for (prop in obj)
       {
-         if ((type=typeof(SFW[prop]))=="function"
-             && (func=SFW[prop])
+         if ((type=typeof(obj[prop]))=="function"
+             && (func=obj[prop])
              && (params = reFuncParams.exec(func.toString())))
          {
             arr.push(type + ": " + prop + "(" + params[1] + ")");
