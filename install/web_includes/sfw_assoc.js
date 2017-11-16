36fdda07658cd1a2a7183c9495321e6085206f0e
Chuck Jungmann <chuckj60@gmail.com>
2017-11-16 11:41:39 -0600

    New class to, perhaps, replace iltable with a more flexible display class.

---
+++ install/web_includes/sfw_assoc.js
@@ -0,0 +1,112 @@
+
+// sfw_assoc.js
+
+/* assoc = Associations object */
+
+(function _init()
+{
+   if ((!("SFW" in window) && setTimeout(_init,100))
+       || SFW.delay_init("sfw_assoc", _init, "tbase"))
+      return;
+
+   if (!SFW.derive(_assoc, "assoc", "tbase"))
+      return;
+
+   function _assoc(actors)
+   {
+      SFW.types["tbase"].call(this,actors);
+      var ths = this;
+      function f(n)
+      {
+         if (n.nodeType==1 && n.tagName.toLowerCase()=="input")
+            return (ths._input=n);
+         return false;
+      }
+      SFW.find_child_matches(actors.anchor,f,true,true);
+
+      this._tbody = actors.anchor.getElementsByTagName("tbody")[0];
+   }
+
+   _assoc.prototype.shadow_transform = function(val)
+   {
+      var fname = this.get_field_name();
+      var shadow = this.add_schema_shadow(fname,val.trim());
+      if (shadow)
+      {
+         SFW.show_string_in_pre(serialize(SFW.xmldoc));
+         var el = this._tbody;
+         if (el)
+            SFW.xslobj.transformFill(el, shadow.attributes[0]);
+         shadow.parentNode.removeChild(shadow);
+      }
+   };
+
+   _assoc.prototype.replot = function(result)
+   {
+      if (!this.do_proxy_override("replot",arguments))
+         this.shadow_transform(this._input.value);
+   };
+
+   _assoc.prototype.get_line_click_action = function()
+   {
+      var field = this.get_schema_field();
+      return field ? field.getAttribute("on_line_click") : null;
+   }
+
+   _assoc.prototype.get_on_add_info = function()
+   {
+      var rval = null;
+      var field = this.get_schema_field();
+      if (field)
+      {
+         var result, url;
+         if ((url=field.getAttribute("on_add")) && (result=this.get_ref_result()))
+            rval = { task:url, result:result};
+      }
+      return rval;
+   };
+
+   _assoc.prototype.process = function(e,t)
+   {
+      // For now, only handle clicks (keyboard handled earlier):
+      if (e.type!="click")
+         return true;
+
+      var table_el = this.widget();
+
+      // Allow base class to process generic buttons
+      if (!SFW.base.prototype.process.call(this,e,t))
+         return false;
+
+      var click_info;
+      while (t && t!=table_el)
+      {
+         var tn = t.tagName.toLowerCase();
+         if (tn=="button")
+         {
+            if (t.name=="add")
+            {
+               click_info = this.get_on_add_info();
+               break;
+            }
+         }
+         else if (t.getAttribute("data-id"))
+         // if (tn=="tr")
+         {
+            if (tn=="tr")
+               click_info=this.get_line_click_info(t);
+            else if (tn=="td")
+               click_info=this.get_cell_click_info(t);
+            else
+               click_info=this.get_el_click_info(t);
+               
+            if (click_info)
+               return this.process_click_info(click_info);
+         }
+         t = t.parentNode;
+      }
+
+      return true;
+   }
+
+})();
