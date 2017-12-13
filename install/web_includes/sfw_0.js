f98294323b65aea1a80bf0d343e5aea84913e651
Chuck Jungmann <chuckj60@gmail.com>
2017-12-13 17:39:59 -0600

    Removed commented-out old update_xmldoc().

--- install/web_includes/sfw_0.js
+++ install/web_includes/sfw_0.js
@@ -1304,19 +1304,6 @@ function init_SFW(callback)
       }
    }
 
-   // function _update_xmldoc(doc, form)
-   // {
-   //    var pagedoc = SFW.xmldoc;
-   //    var arr = doc.selectNodes("/*/*[@rndx][@type='update']");
-   //    for (var i=0, stop=arr.length; i<stop; ++i)
-   //    {
-   //       var result = arr[i];
-   //       var target = _get_target_result(result, pagedoc);
-   //       if (target)
-   //          _put_row_into_target(target, result.selectSingleNode("*"));
-   //    }
-   // }
-
    /**
     * Now does both delete and add/replace updates.
     *
