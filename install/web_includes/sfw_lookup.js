6dfb71eb8de839beef36e086ca2fb4d602a76b49
Chuck Jungmann <chuckj60@gmail.com>
2018-03-06 14:00:01 -0600

    Fixed process for updating linked data, put code in rebuild_content() to be called by both replot() and preview_result().

--- install/web_includes/sfw_lookup.js
+++ install/web_includes/sfw_lookup.js
@@ -23,20 +23,78 @@
       return label ? label.getAttribute("for") : "";
    };
 
+   _lookup.prototype.get_form_label = function()
+   {
+      var tag_name, matched=false, e = this.top();
+      while (e && e.nodeType==1)
+      {
+         if ((matched=(e.tagName.toLowerCase()=="p" && class_includes(e,"form-row"))))
+            break;
+
+         e = e.parentNode;
+      }
+
+      if (matched)
+      {
+         var labels = e.getElementsByTagName("label");
+         if (labels)
+            return labels[0];
+      }
+
+      return null;
+   };
+
+   _lookup.prototype.get_field_name = function()
+   {
+      var label = this.get_form_label();
+      if (label)
+         return label.getAttribute("for");
+      return null;
+   };
+
+   function _rebuild_lookup(host, xrow, fieldname)
+   {
+      xrow.setAttribute("lookup-field-match", fieldname);
+      SFW.xslobj.transformFill(host, xrow);
+      xrow.removeAttribute("lookup-field-match");
+   }
+
+   _lookup.prototype.get_form_xrow = function()
+   {
+      var top, xpath, node = null;
+      if ((top=this.top()) && (xpath=top.getAttribute("data-path")))
+         node = SFW.xmldoc.selectSingleNode(xpath);
+      return node;
+   };
+
+   _lookup.prototype.rebuild_content = function()
+   {
+      var widget, host, xrow, fname;
+      if ((widget=this.widget())
+          && (host=widget.parentNode)
+          && (xrow=this.get_form_xrow())
+          && (fname=this.get_field_name()))
+         _rebuild_lookup(host,xrow,fname);
+   };
+
+
+   _lookup.prototype.preview_result = function(returned_doc, child)
+   {
+      this.rebuild_content();
+   };
+
    _lookup.prototype.replot = function(result)
    {
-      var top = this.top();
-      var host = top.parentNode;
-      var xpath = top.getAttribute("data-path");
-      var node = SFW.xmldoc.selectSingleNode(xpath);
+      this.rebuild_content();
+      // var top = this.top();
+      // var host = top.parentNode;
+      // var xpath = top.getAttribute("data-path");
+      // var node = SFW.xmldoc.selectSingleNode(xpath);
+      // var fname = this.get_field_name();
 
-      node.setAttribute("lookup-field-match", field_name_from_host(host));
-
-      this.pre_transform();
-      SFW.xslobj.transformFill(host, node);
-      this.post_transform();
-
-      node.removeAttribute("lookup-field-match");
+      // this.pre_transform();
+      // _rebuild_lookup(host, node, fname);
+      // this.post_transform();
    };
 
 
