1f5f5dc00b754835a6b4a690704b7b1fdc6e41d3
Chuck Jungmann <chuckj60@gmail.com>
2018-07-15 13:54:05 -0500

    Removed src directory, now that it's moved to project SchemaServer.

--- src/xmlutils.cpp
+++
@@ -1,9 +0,0 @@
-#include "xmlutils.hpp"
-
-void print_xml_head(const char *stylesheet_name, FILE *fp)
-{
-   fputs("<?xml version=\"1.0\" ?>\n", fp);
-   if (stylesheet_name)
-      fprintf(fp,"<?xml-stylesheet type=\"text/xsl\" href=\"%s\" ?>\n",
-              stylesheet_name);
-}
--- src/xmlutils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef XMLUTILS_HPP
-#define XMLUTILS_HPP
-
-#include "istdio.hpp"
-
-void print_xml_head(const char *stylesheet_name=nullptr, FILE *fp=stdout);
-
-#endif
-
